1f2e3f4b2031f3565892a5bd5394f1303cec5f54
Thabang Selowa <mohale20cobra@gmail.com>
2024-09-19 17:26:15 +0200

    Add state counter reducer

--- script.js
+++ script.js
@@ -7,3 +7,73 @@ const initialState = {
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
 const RESET = 'RESET';
+
+// Action creators
+const increment = () => {
+    console.log('Action: INCREMENT');
+    return { type: INCREMENT };
+};
+
+const decrement = () => {
+    console.log('Action: DECREMENT');
+    return { type: DECREMENT };
+};
+
+const reset = () => {
+    console.log('Action: RESET');
+    return { type: RESET };
+};
+
+// State counterReducer
+const counterReducer = (state = initialState, action) => {
+    console.log('Reducer received action:', action);
+    switch (action.type) {
+        case INCREMENT:
+            return { ...state, count: state.count + 1 };
+        case DECREMENT:
+            return { ...state, count: state.count - 1 };
+        case RESET:
+            return { ...state, count: 0 };
+        default:
+            return state;
+    }
+};
+
+// Store class
+class Store {
+    constructor(reducer) {
+        this.reducer = reducer;
+        this.state = this.reducer(undefined, {});
+        this.listeners = [];
+    }
+
+    getState() {
+        return this.state;
+    }
+
+    dispatch(action) {
+        this.state = this.reducer(this.state, action);
+        this.listeners.forEach(listener => listener(this.state));
+    }
+
+    subscribe(listener) {
+        this.listeners.push(listener);
+        return () => {
+            this.listeners = this.listeners.filter(l => l !== listener);
+        };
+    }
+}
+
+// Create a store instance
+const store = new Store(counterReducer);
+
+// Subscribe to state changes
+store.subscribe((state) => {
+    console.log('Current state:', state);
+});
+
+// Dispatch some actions
+store.dispatch(increment()); // Current state: { count: 1 }
+store.dispatch(increment()); // Current state: { count: 2 }
+store.dispatch(decrement()); // Current state: { count: 1 }
+store.dispatch(reset());      // Current state: { count: 0 }
